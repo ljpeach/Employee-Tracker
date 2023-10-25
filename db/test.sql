e116e82b4d323ec4eb5439a42a531840debec6c3
Liam Peachey <liamjpeachey@gmail.com>
2023-10-25 01:53:46 -0500

    Deleted test sql file

--- db/test.sql
+++
@@ -1,4 +0,0 @@
-SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, CONCAT(manage.first_name, ' ', manage.last_name) AS manager
-FROM employee LEFT JOIN employee AS manage ON employee.manager_id = manage.id
-JOIN role ON employee.role_id = role.id
-JOIN department ON role.department_id = department.id
